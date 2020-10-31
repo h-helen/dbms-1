238a7bb1984aeb5d1aee65b78816940a03de0ad6
黄皓 <18301005@bjtu.edu.cn>
2020-10-31 22:49:14 +0800

    新建项目

---
+++ dbms/dbms/dbmsView.cpp
@@ -0,0 +1,128 @@
+﻿
+// dbmsView.cpp: CdbmsView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "dbms.h"
+#endif
+
+#include "dbmsDoc.h"
+#include "dbmsView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CdbmsView
+
+IMPLEMENT_DYNCREATE(CdbmsView, CView)
+
+BEGIN_MESSAGE_MAP(CdbmsView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CdbmsView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CdbmsView 构造/析构
+
+CdbmsView::CdbmsView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CdbmsView::~CdbmsView()
+{
+}
+
+BOOL CdbmsView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CdbmsView 绘图
+
+void CdbmsView::OnDraw(CDC* /*pDC*/)
+{
+	CdbmsDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CdbmsView 打印
+
+
+void CdbmsView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CdbmsView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CdbmsView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CdbmsView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+void CdbmsView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CdbmsView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CdbmsView 诊断
+
+#ifdef _DEBUG
+void CdbmsView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CdbmsView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CdbmsDoc* CdbmsView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CdbmsDoc)));
+	return (CdbmsDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CdbmsView 消息处理程序
---
+++ dbms/dbms/dbmsView.h
@@ -0,0 +1,52 @@
+﻿
+// dbmsView.h: CdbmsView 类的接口
+//
+
+#pragma once
+
+
+class CdbmsView : public CView
+{
+protected: // 仅从序列化创建
+	CdbmsView() noexcept;
+	DECLARE_DYNCREATE(CdbmsView)
+
+// 特性
+public:
+	CdbmsDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CdbmsView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // dbmsView.cpp 中的调试版本
+inline CdbmsDoc* CdbmsView::GetDocument() const
+   { return reinterpret_cast<CdbmsDoc*>(m_pDocument); }
+#endif
+
